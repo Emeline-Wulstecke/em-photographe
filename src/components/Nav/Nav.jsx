453e87fa9e6fd6edef146143ef0dfc432eefa488
emelinew1 <emeline.wulstecke@gmail.com>
2024-06-07 15:54:33 +0200

    Installation React & Redux

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import './nav.css';
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav id ="nav">
+        <ul>
+        <li>Instagram</li>
+      </ul>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
