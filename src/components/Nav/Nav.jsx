5e9113923b6b19f261f783f8c3e11d6fe1f9f232
emelinew1 <emeline.wulstecke@gmail.com>
2024-06-10 12:43:18 +0200

    css nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,23 @@
-import './nav.css';
 import React from 'react';
+import './nav.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Banner from '../../img/banner.jpg';
 
 const Nav = () => {
   return (
-    <nav id ="nav">
-        <ul>
-        <li>Instagram</li>
-      </ul>
+    <nav id="nav">
+     <h1>Capturing your special moments</h1>
       <ul>
         <li>Home</li>
         <li>About</li>
         <li>Portfolio</li>
         <li>Contact</li>
+        <li><FontAwesomeIcon icon={faInstagram} /></li>
       </ul>
+        <img src={Banner} alt="Un papa et sa fille dans le désert de Marrakech" />
     </nav>
   );
-}
+};
 
 export default Nav;
